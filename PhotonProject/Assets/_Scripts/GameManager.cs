c1fe5329e0c0777ce3bcefe0ce67adaef7e1e814
AMIR\amir <choubaniAmir@bitbucket.org>
2017-05-19 23:37:11 +0200

    update1

---
+++ PhotonProject/Assets/_Scripts/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Com.MyCompany.MyGame
+{
+
+
+    public class GameManager : MonoBehaviour
+    {
+        #region Photon Messages
+
+
+        /// <summary>
+        /// Called when the local player left the room. We need to load the launcher scene.
+        /// </summary>
+        public void OnLeftRoom()
+        {
+            SceneManager.LoadScene(0);
+            
+        }
+
+
+        #endregion
+
+
+        #region Public Methods
+
+
+        public void LeaveRoom()
+        {
+            PhotonNetwork.LeaveRoom();
+        }
+
+
+        #endregion
+
+    }
+
+}
